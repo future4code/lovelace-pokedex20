377baa03c68e7fd4cd140cde5b0e9c9b2fd45f1b
GuilhermeMedici <mailguigs@gmail.com>
2021-08-23 19:11:55 -0300

    Criadas as páginas básicas, criado componente destinado pra rotas, funcionando de maneira correta

---
+++ pokedex20/src/pages/DetailsPage/DetailsPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function DetailsPage () {
+    
+    return (
+
+        <div>
+            <p>DetailsPage</p>
+        </div>
+    )
+}
+
+export default DetailsPage;
